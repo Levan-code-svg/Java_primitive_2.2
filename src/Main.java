0bc1daef43a28c896e9aa9852149cae283ab478c
Levan <kft77736@gmail.com>
2025-01-28 22:19:55 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,11 @@ public class Main {
 
         int initialAccount = 790;
         int replenishmentAccount = 3450;
-
         int bonus;
 
-        if (replenishmentAccount > 1000) {
-            bonus = replenishmentAccount / 100;
-        } else {
-            bonus = 0;
-        }
+        if (replenishmentAccount > 1000) {bonus = replenishmentAccount / 100;} else { bonus = 0;}
 
         System.out.println(" Общий баланс счета: " + (initialAccount + replenishmentAccount + bonus) + " руб.");
-    System.out.println("Бонусный счет:" + bonus + " руб.");
+        System.out.println("Бонусный счет:" + bonus + " руб.");
     }
 }
