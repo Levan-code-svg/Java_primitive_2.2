4bb87ee1f439399416aa2cd3e8623536e19936ea
Levan <kft77736@gmail.com>
2025-02-04 23:47:35 +0300

    change added

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
         int initialAccount = 790;
         int replenishmentAccount = 3450;
         int bonus;
 
-        if ( replenishmentAccount > 1000 ) {
+        if (replenishmentAccount > 1000) {
             bonus = replenishmentAccount / 100;
-        }
-        else {
+        } else {
             bonus = 0;
         }
 
-        System.out.println(" Общий баланс счета: " + (initialAccount + replenishmentAccount + bonus) + " руб.");
-        System.out.println("Бонусный счет:" + bonus + " руб.");
+        System.out.println("Общий баланс счета: " + (initialAccount + replenishmentAccount + bonus) + " руб.");
+        System.out.println("Бонусный счет: " + bonus + " руб.");
     }
 }
